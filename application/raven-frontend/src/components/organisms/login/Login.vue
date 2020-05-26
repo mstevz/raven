a138b9da5ac3736a2eb2a609b601287bbbd39c1c
mstevz <mstevz.dev@gmail.com>
2020-05-26 18:55:31 +0100

    /feature Login Page

---
+++ application/raven-frontend/src/components/organisms/login/Login.vue
@@ -0,0 +1,75 @@
+<template>
+    <section class="login-area">
+        <div>
+            <img src="@/assets/ravenlogo.svg" style="max-width:100px" alt="">
+
+            <form id="loginForm" action="index.html" method="post">
+
+                <label for="">Username</label>
+                <input type="text" name="" value="" />
+
+                <label for="">Password</label>
+                <input type="password" name="" value="" />
+
+                <button type="button" name="button">Login</button>
+
+                <button type="button" name="button">Sign Up</button>
+            </form>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'login'
+}
+</script>
+
+<style lang="scss" scoped>
+    .login-area {
+        display:flex;
+        flex-direction:column;
+
+        height:100vh;
+        width:100vw;
+
+        background-color: #c1a57b;
+
+        align-items: center;
+        justify-content: center;
+
+        div {
+            width:100%;
+            text-align: center;
+
+
+            #loginForm {
+                display: flex;
+                flex-direction: column;
+
+                text-align: left;
+
+                padding:0px 20px;
+                padding-bottom:50px;
+                border-radius:10px;
+
+                input[type="text"],
+                input[type="password"],
+                button{
+                    padding:8px;
+                    margin-top:5px;
+                    margin-bottom:5px;
+                }
+
+                button {
+                    background-color: #1a1a1a;
+                    color: #fff;
+                    border-style:none;
+                    cursor:pointer;
+
+                }
+            }
+        }
+
+    }
+</style>
--- application/raven-frontend/src/components/views/Home.vue
+++ application/raven-frontend/src/components/views/Home.vue
@@ -1,14 +1,32 @@
 <template>
     <div>
-        Welcome to the mighty home!
+
+        <section v-if="isLoggedIn">
+            Welcome to the mighty home!
+        </section>
+        <section v-else>
+            <login />
+        </section>
+
+
+
     </div>
+
+
 </template>
 
 <script>
+import Login from '@/components/organisms/login/Login.vue';
 
 export default {
     name: 'home',
     components: {
+        Login
+    },
+    data: function() {
+        return {
+            isLoggedIn: false
+        }
     }
 }
 </script>
